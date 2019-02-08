eea47cf6e7fbc8b114b08ecd250a990f5ecd3ac7
marcellus reinaldo <marcellus.jodihardja@traveloka.com>
2019-02-08 18:00:32 +0700

    add serving component

---
+++ {{ cookiecutter._repo_name }}/src/models/serving/main.py
@@ -0,0 +1,15 @@
+
+class TrainedModel:
+    def __init__(self, model_dir):
+        pass
+
+    def predict(self, request):
+        return ""
+
+    def health(self):
+        return '200 OK'
+
+
+    def info(self):
+        return ""
+        
