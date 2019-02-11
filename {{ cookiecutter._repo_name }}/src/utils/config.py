bb6c20cc742ac519208ecf051eb12a77dde80b38
Eduard Chai <eduard.chai@traveloka.com>
2019-02-12 03:00:52 +0800

    update cookiecutter to reflect changes on other components

--- {{ cookiecutter._repo_name }}/src/utils/config.py
+++ {{ cookiecutter._repo_name }}/src/utils/config.py
@@ -22,8 +22,8 @@ def parse_uri(uri):
     return parsed.netloc, path
 
 @retry(retry_on_exception=retry_if_exception, stop_max_attempt_number=3, wait_random_min=1000, wait_random_max=2000)
-def get_auth0_config(auth0_config_uri, project_id='tvlk-data-dev-179204',location_id="global",
-                key_ring_id='rm-config', crypto_key_id='rm-config'):
+def get_auth0_config(auth0_config_uri, project_id='tvlk-data-mlplatform-prod',location_id='global',
+                key_ring_id='raring-meerkat-common', crypto_key_id='auth0-dev'):
     bucket_name, filePath = parse_uri(auth0_config_uri)
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(bucket_name)
