c2b6da682c36987774bdf94176438acf23615cff
Eduard Chai <eduard.chai@traveloka.com>
2019-04-03 12:00:53 +0800

    update to sync with latest changes from rmi-cli

--- {{ cookiecutter._repo_name }}/.jenkinsfile/Jenkinsfile.build.groovy
+++ {{ cookiecutter._repo_name }}/.jenkinsfile/Jenkinsfile.build.groovy
@@ -3,32 +3,29 @@
 
 // GKE deployment setting for the application
 def appName = "{{ cookiecutter._repo_name }}"
-def environment = "stg" //rmi build always in stage
+def environment = "prod"
 
-def progressDeadlineSeconds = 180
+def progressDeadlineSeconds = 300
 
 coreJenkinsWorkerNode(
-        environment: environment,
-        appName: appName,
-        slackChannel: "#data-jenkins-hub",
-        // The gke cluster where the training will be executed
-        // Set to default
-        clusterProjectId: "tvlk-data-mlplatform-prod",
-        clusterName: "rm-training",
-        clusterZone: "asia-southeast1-a",
-
-        additionalContainers:[
-            containerTemplate(name: 'python', image: 'python:3.6', ttyEnabled: true, command: 'python')
-        ]
+    environment: environment,
+    appName: appName,
+    slackChannel: "#data-jenkins-hub",
+    // The gke cluster where the training will be executed
+    // Set to default
+    clusterProjectId: "tvlk-data-mlplatform-prod",
+    clusterName: "rm-training",
+    clusterZone: "asia-southeast1-a",
+    jenkinsSlaveServiceAccountCredentialId: "rm-jenkins-bot-prod",
+    additionalContainers:[
+        containerTemplate(name: 'python', image: 'python:3.6', ttyEnabled: true, command: 'python')
+    ]
 ){
     stage('Checkout') {
         checkout scm
     }
 
     def commitId = sh(returnStdout: true, script: "git rev-parse HEAD").trim().take(6)
-    def plainTextName = "raring-meerkat-model-builder.json"
-    def cipherTextName = "raring-meerkat-model-builder.json.enc"
-    def cipherTextLocation = "gs://raring-meerkat-common-kms/${cipherTextName}"
 
     currentBuild.displayName = "#$BUILD_NUMBER ${commitId}"
     currentBuild.description = "Commit Hash: ${commitId}"
@@ -46,21 +43,15 @@ coreJenkinsWorkerNode(
     // }
 
     stage('Build') {
-        withCredentials([string(credentialsId: 'rmi-access-token', variable: 'rmiAccessToken')]) {
-            sh "gsutil cp ${cipherTextLocation} ."
-            sh """
-                gcloud kms decrypt \
-                    --location global \
-                    --keyring raring-meerkat-common \
-                    --key model-builder \
-                    --ciphertext-file ${cipherTextName} \
-                    --plaintext-file ${plainTextName}
-            """
-            sh "wget https://storage.googleapis.com/rmi-releases/master-cd/latest/linux/amd64/rmi.tar.gz"
+        withCredentials([
+            string(credentialsId: 'rmi-access-token-prod', variable: 'RMI_ACCESS_TOKEN'),
+            file(credentialsId: 'rm-jenkins-bot-prod', variable: 'GOOGLE_APPLICATION_CREDENTIALS')
+        ]) {
+            sh "wget https://storage.googleapis.com/rmi-releases/latest/linux/amd64/rmi.tar.gz"
             sh "tar xzf rmi.tar.gz"
             sh "chmod +x rmi"
-            sh "GOOGLE_APPLICATION_CREDENTIALS=${plainTextName} ./rmi version"
-            sh "RMI_ACCESS_TOKEN=${rmiAccessToken} GOOGLE_APPLICATION_CREDENTIALS=${plainTextName} ./rmi build"
+            sh "./rmi version"
+            sh "./rmi build"
         }
     }
 }
